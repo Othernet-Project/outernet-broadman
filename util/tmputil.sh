da83838fdfd250715fa6e160847ba94937e2a9ef
Branko Vukelic <branko@brankovukelic.com>
2015-05-03 12:03:36 +0200

    Added function to create temporary file

--- util/tmputil.sh
+++ util/tmputil.sh
@@ -37,3 +37,41 @@ mktmpdir() {
     mkdir -p "$dname"
     echo $dname
 }
+
+
+# mktmppath(pfx, $ext)
+# ==============
+#
+# Createa blank temporary file and echoes the path. The temporary file is 
+# created under the system temporary directory. The file name is based on 
+# current time and optional prefix $pfx.
+#
+# Default prefix is 'tmp'
+#
+# Optionally, extension may be supplied. The extension defaults to blank.
+#
+# Temporary file is not automatically cleaned up (except when the operating 
+# system performs this task).
+#
+# Example:
+#
+#     $ mktmppath foo
+#     /tmp/foo-1429716349
+#
+#     $ mktmppath foo js
+#     /tmp/foo-1429716349.js
+#
+#     $ mktmppath
+#     /tmp/tmp-1429716349
+#
+mktmppath() {
+    pfx=${1:-tmp}
+    ext=$2
+    fname=${SYSTEMP}/${pfx}-$(date +%s)
+    if [ ! -z "$ext" ];
+    then
+        fname=${fname}.${ext}
+    fi
+    touch "$fname"
+    echo "$fname"
+}
