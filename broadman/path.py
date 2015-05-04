4ee175cde902ab3392c6e66a8ad8088f6ec09dc9
Branko Vukelic <branko@brankovukelic.com>
2015-05-04 18:42:19 +0200

    Removed unused import

--- broadman/path.py
+++ broadman/path.py
@@ -12,7 +12,6 @@ from __future__ import division
 
 import re
 import os
-import itertools
 
 # Definitive rule for where to get the content pool directory
 POOLDIR = os.environ.get('OUTERNET_CONTENT', '.').rstrip(os.sep)
