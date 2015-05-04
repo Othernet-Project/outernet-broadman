529e6609cf38e18f976a99c5665b812343c026dd
Branko Vukelic <branko@brankovukelic.com>
2015-05-04 15:29:42 +0200

    Added copyright notice

--- broadman/zips.py
+++ broadman/zips.py
@@ -1,6 +1,11 @@
 """
 Functions for working with zipfiles
 
+Copyright 2015, Outernet Inc.
+Some rights reserved.
+
+This software is free software licensed under the terms of GPLv3. See COPYING
+file that comes with the source code, or http://www.gnu.org/licenses/gpl.txt.
 """
 
 import os
