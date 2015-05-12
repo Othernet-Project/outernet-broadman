d2d91fa4b6620043ce1377f65a08be801232c21b
Branko Vukelic <branko@brankovukelic.com>
2015-05-12 16:41:55 +0200

    Added options for modifying metadta during update

--- broadman/update.py
+++ broadman/update.py
@@ -14,10 +14,14 @@ import os
 
 import conz
 import outernet_metadata.validate as validate
+import outernet_metadata.values as values
 
 from . import git
 from . import path
 from . import jsonf
+
+
+IMAGE_EXTS = ['.jpg', '.jpeg', '.png', '.gif', '.svg', '.ico']
 
 
 cn = conz.Console()
@@ -39,15 +43,37 @@ def revert(cid):
         cn.pverr(cid, 'there is nothing to revert')
 
 
-def update(cid):
+def add_optional_keys(data):
+    for k, v in values.DEFAULTS.items():
+        if k not in data:
+            data[k] = v
+    if 'broadcast' not in data:
+        data['broadcast'] = '$BROADCAST'
+
+
+def update_image_count(p, data):
+    matcher = lambda p: os.path.splitext(p)[1].lower() in IMAGE_EXTS
+    data['images'] = path.countwalk(p, matcher)
+    assert data['images'] >= 0, 'Expected image count to be 0 or more'
+
+
+def update(cid, add_optional=False, update_images=False, reformat=False):
     cdir = path.contentdir(cid)
+    infopath = path.infopath(cdir)
     if not git.has_changes(cdir):
         cn.pverr(cid, 'nothing to update')
         return
     try:
-        data = jsonf.load(path.infopath(cdir))
+        data = jsonf.load(infopath)
     except jsonf.LoadError:
         raise ValueError('invalid metadata file')
+    # Modify data if needed
+    if add_optional:
+        add_optional_keys(data)
+    if update_images:
+        update_image_count(data)
+    if any([add_optional, update_images, reformat]):
+        jsonf.save(infopath, data)
     # Validate metadata
     ret = validate.validate(data)
     if ret:
@@ -73,12 +99,20 @@ def main():
                         help='content ID or content directory path')
     revgrp = parser.add_argument_group('Rollback options')
     revopt = revgrp.add_mutually_exclusive_group()
-    revopt.add_argument('--reset', '-r', action='store_true',
+    revopt.add_argument('--reset', '-R', action='store_true',
                         help='reset all changes instead of updating (cannot '
                         'be used with --revert)')
     revopt.add_argument('--revert', '-v', action='store_true',
                         help='revert last set of changes (cannot be used with '
                         'reset)')
+    metagrp = parser.add_argument_group('Metadata options (ignored '
+                                        'when --reset or --revert are used)')
+    metagrp.add_argument('--add-optional', '-a', action='store_true',
+                         help='add all misssing optional keys')
+    metagrp.add_argument('--update-images', '-i', action='store_true',
+                         help='update image count based on actual content')
+    metagrp.add_argument('--reformat', '-r', action='store_true',
+                         help='reformat the metadata file')
     args = parser.parse_args()
 
     if cn.interm:
@@ -95,7 +129,9 @@ def main():
     elif args.revert:
         fn = revert
     else:
-        fn = update
+        fn = lambda cid: update(cid, add_optional=args.add_optional,
+                                update_images=args.update_images,
+                                reformat=args.reformat)
 
     for cid in src:
         cid = path.cid(cid.strip())
