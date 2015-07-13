be929751ba21f2136166c60427e83198f610a74e
Ben Ennis <ben@outernet.is>
2015-07-13 09:14:08 -0700

    initial commit of conversion

--- broadman/git.py
+++ broadman/git.py
@@ -21,6 +21,16 @@ MSG_MARKER = '[OBM]'
 AUTHOR = Actor("Outernet Broadman", "apps@outernet.is")
 
 
+class Git():
+    def __init__(self):
+        repo = Repo(abspath(path.POOLDIR))
+        index = repo.index
+        self.add = index.add
+        self.git = repo.git
+        self.commit = index.commit
+        self.remove = index.remove
+
+
 def init():
     p = path.POOLDIR
     git = Repo.init(p)
@@ -31,88 +41,107 @@ def init():
     git.index.commit('Initialized content pool', author=AUTHOR)
 
 
-class Git():
-    def __init__(self):
-        repo = Repo(abspath(path.POOLDIR))
-        index = repo.index
-        self.add = index.add
-        self.git = repo.git
-        self.commit = index.commit
-        self.remove = index.remove
+def has_changes(p):
+    g = Git()
+    """ Check whether some path contains changes """
+    return g.git.status(p, s=True)
 
-    def has_changes(self, p):
-        """ Check whether some path contains changes """
-        return self.git.status(p, s=True)
 
-    def get_history(self, p):
-        """ Get all commit hashes for a given path as a list """
-        hashes = self.git.log(pretty='format:%H')
-        return hashes.split('\n')
+def get_history(p):
+    g = Git()
+    """ Get all commit hashes for a given path as a list """
+    hashes = g.git.log(pretty='format:%H')
+    return hashes.split('\n')
 
-    def commit(self, p, action, msg=None, extra_data=[], noadd=False):
-        if not noadd:
-            self.add(p)
-        cid = path.cid(p)
-        if not cid:
-            cid = 'BACKLOG'
-        cmsg = [MSG_MARKER, action, cid]
-        cmsg.extend(extra_data)
-        cmsg = ' '.join(cmsg)
-        if msg:
-            cmsg += '\n\n' + msg
-        self.commit(cmsg)
 
-    def commit_import(self, p):
-        self.commit(p, action='IMP', msg='Imported new content')
+def commit(p, action, msg=None, extra_data=[], noadd=False):
+    g = Git()
+    if not noadd:
+        g.add(p)
+    cid = path.cid(p)
+    if not cid:
+        cid = 'BACKLOG'
+    cmsg = [MSG_MARKER, action, cid]
+    cmsg.extend(extra_data)
+    cmsg = ' '.join(cmsg)
+    if msg:
+        cmsg += '\n\n' + msg
+    g.commit(cmsg)
 
-    def commit_add_to_server(self, p, server):
-        cid = path.cid(p)
-        msg = 'Added {} -> {}'.format(cid, server)
-        self.commit(p, action='ADD', msg=msg, extra_data=[server])
 
-    def commit_remove_from_server(self, p, server):
-        self.remove([p], cached=True)
-        cid = path.cid(p)
-        msg = 'Removed {} <- {}'.format(cid, server)
-        self.commit(p, action='DEL', msg=msg, extra_data=[server], noadd=True)
+def commit_import(p):
+    commit(p, action='IMP', msg='Imported new content')
 
-    def commit_update(self, p):
-        has_history = len(self.get_history(p)) > 0
-        self.add(p)
-        changes = self.has_changes(p)
-        if has_history:
-            msg = 'Files changed:\n\n{}'.format(changes)
-            action = 'UPD'
-        else:
-            msg = 'Files added:\n\n{}'.format(changes)
-            action = 'NEW'
-        self.commit(p, action, msg=msg, noadd=True)
 
-    def commit_backlog(self, processed):
-        msg = 'Backlog processed:\n\n{}'.format('\n'.join(processed))
-        self.commit(path.BROADCAST, action='BKL', msg=msg)
+def commit_add_to_server(p, server):
+    cid = path.cid(p)
+    msg = 'Added {} -> {}'.format(cid, server)
+    commit(p, action='ADD', msg=msg, extra_data=[server])
 
-    def revert(self, p):
-        """ Revert given path to specified hash """
-        history = self.get_history(p)
-        print(history)
-        print(history[1])
-        if len(history) < 2:
-            raise ValueError('nothing to do')
-        self.git.checkout(history[1], p=True)
-        msg = 'Reverted {} to previous state'.format(history[1])
-        self.commit(p, 'REV', msg=msg)
 
-    def reset(self, p):
-        """ Remove any changes on path """
-        history = self.get_history(p)
-        if len(history) < 1:
-            raise ValueError('nothing to do')
-        self.git.clean(f=True, d=True, p=True)
-        self.git.checkout(history[0], p=True)
+def commit_remove_from_server(p, server):
+    g = Git()
+    g.remove([p], cached=True)
+    cid = path.cid(p)
+    msg = 'Removed {} <- {}'.format(cid, server)
+    commit(p, action='DEL', msg=msg, extra_data=[server], noadd=True)
 
-    def remove(self, p):
-        """ Remove directory and all contents """
-        self.remove([p])
-        msg = 'Removed {} from pool'.format(p)
-        self.commit(p, 'REM', msg)
+
+def commit_update(p):
+    g = Git()
+    has_history = len(get_history(p)) > 0
+    g.add(p)
+    changes = has_changes(p)
+    if has_history:
+        msg = 'Files changed:\n\n{}'.format(changes)
+        action = 'UPD'
+    else:
+        msg = 'Files added:\n\n{}'.format(changes)
+        action = 'NEW'
+    commit(p, action, msg=msg, noadd=True)
+
+
+def commit_backlog(processed):
+    msg = 'Backlog processed:\n\n{}'.format('\n'.join(processed))
+    commit(path.BROADCAST, action='BKL', msg=msg)
+
+
+def revert(p):
+    """ Revert given path to specified hash """
+    g = Git()
+    history = get_history(p)
+    print(history)
+    print(history[1])
+    if len(history) < 2:
+        raise ValueError('nothing to do')
+    g.git.checkout(history[1], p=True)
+    msg = 'Reverted {} to previous state'.format(history[1])
+    commit(p, 'REV', msg=msg)
+
+
+def reset(p):
+    g = Git()
+    """ Remove any changes on path """
+    history = get_history(p)
+    if len(history) < 1:
+        raise ValueError('nothing to do')
+    g.git.clean(f=True, d=True, p=True)
+    g.git.checkout(history[0], p=True)
+
+
+def remove(p):
+    g = Git()
+    """ Remove directory and all contents """
+    g.remove([p])
+    msg = 'Removed {} from pool'.format(p)
+    commit(p, 'REM', msg)
+
+
+def check_dir(dir):
+    """ GitPython doesn't like absolute paths
+    We check if the root of the provided path is the same as path.POOLDIR. If
+    it is, we remove it. """
+    p = path.POOLDIR
+    if dir[:len(p)] == p:
+        return dir[len(p):]
+
